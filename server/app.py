176f9a7eb0003cd434068b63bda8a4fc3c60b866
Barasa-Micah <barasamica@gmail.com>
2023-09-19 12:00:47 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return f'<h1> Printed String</h1>'
+
+@app.route('/<int:count>')
+def count(count):
+    return f'<h1>Count here {count}</h1>'
+
+
+@app.route('/int:num1>/<operator>/<int:num2>')
+def math(num1, operator, num2):
+    if operator== '+':
+        result = num1 + num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
+    else:
+        result = 'Invalid operator'
+
+    return f'<h1>Your is answer is {result} </h1> '
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
